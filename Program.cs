972180272145d1c31c70bd652f64723922aecb51
Nathan Dimitriades <natephor@outlook.com>
2024-11-28 18:50:58 +0200

    refactor: rename to Row and Column

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿string? userInput;
+// Declared for readability
+var fountainLocation = (0, 2);
+var mazeEntrancePos = (0, 0);
 var availableMazeSizes = new Dictionary<int, int>
 {
     { 1, 4 },
@@ -6,24 +9,23 @@ var availableMazeSizes = new Dictionary<int, int>
     { 3, 8 }
 };
 int userChoice;
+var random = new Random();
+
+Console.WriteLine("Enter the maze!");
 do
 {
-    Console.WriteLine("Do you want to play in a small, medium or large game? ");
+    Console.WriteLine("Do you want to play in a small, medium or large maze? ");
     Console.WriteLine("1. Small");
     Console.WriteLine("2. Medium");
     Console.WriteLine("3. Large");
     userInput = Console.ReadLine();
 } while (!int.TryParse(userInput, out userChoice) || userChoice is < 1 or > 3);
 
-// used for maelstrom sneezes (all maelstroms sneeze due to allergies!)
-var random = new Random();
 var sizeOfMaze = availableMazeSizes[userChoice];
 
-var fountainLocation = (0, 2);
-
+// For large mazes we need 2 maelstroms
 var maelstromCount = sizeOfMaze == 8 ? 2 : 1;
-var maelstroms = new (int y, int x)[maelstromCount];
-(int y, int x) playerPos = (0, 0);
+var maelstroms = new (int row, int column)[maelstromCount];
 
 for (var i = 0; i < maelstromCount; i++)
 {
@@ -31,39 +33,40 @@ for (var i = 0; i < maelstromCount; i++)
 }
 
 var isFountainActivated = false;
-var playerIsAtExit = playerPos is { y: 0, x: 0 };
+(int row, int column) currentPlayerPos = (0, 0);
+var playerIsAtExit = currentPlayerPos == mazeEntrancePos;
 while (!isFountainActivated || !playerIsAtExit)
 {
-    DisplayLocationMessage(playerPos);
+    DisplayLocationMessage(currentPlayerPos);
     var userAction = GetUserAction();
     switch (userAction)
     {
         case "move east":
-            if (playerPos.x == sizeOfMaze - 1)
+            if (currentPlayerPos.column == sizeOfMaze - 1)
                 Console.WriteLine("You've hit a wall. Cannot move east.");
             else
-                playerPos.x += 1;
+                currentPlayerPos.column += 1;
             break;
         case "move north":
-            if (playerPos.y == 0)
+            if (currentPlayerPos.row == 0)
                 Console.WriteLine("You've hit a wall. Cannot move north.");
             else
-                playerPos.y -= 1;
+                currentPlayerPos.row -= 1;
             break;
         case "move south":
-            if (playerPos.y == sizeOfMaze - 1)
+            if (currentPlayerPos.row == sizeOfMaze - 1)
                 Console.WriteLine("You've hit a wall. Cannot move south.");
             else
-                playerPos.y += 1;
+                currentPlayerPos.row += 1;
             break;
         case "move west":
-            if (playerPos.x == 0)
+            if (currentPlayerPos.column == 0)
                 Console.WriteLine("You've hit a wall. Cannot move west.");
             else
-                playerPos.x -= 1;
+                currentPlayerPos.column -= 1;
             break;
         case "enable fountain":
-            if (playerPos == fountainLocation)
+            if (currentPlayerPos == fountainLocation)
                 if (isFountainActivated)
                     Console.WriteLine("You've already activated the fountain");
                 else
@@ -77,24 +80,24 @@ while (!isFountainActivated || !playerIsAtExit)
             break;
     }
 
-    playerIsAtExit = playerPos is { y: 0, x: 0 };
-    if (maelstroms.Any(m => m == playerPos))
+    playerIsAtExit = currentPlayerPos == mazeEntrancePos;
+    if (maelstroms.Any(m => m == currentPlayerPos))
     {
         Console.WriteLine("Oh no! you ran into a maelstrom!");
-        var previousMaelstromLocation = playerPos;
-        var nextMaelstromLocation = playerPos;
-        playerPos.y = Math.Clamp(playerPos.y + 2, 0, sizeOfMaze);
-        playerPos.x = Math.Clamp(playerPos.x + 1, 0, sizeOfMaze);
+        var previousMaelstromLocation = currentPlayerPos;
+        var nextMaelstromLocation = currentPlayerPos;
+        currentPlayerPos.row = Math.Clamp(currentPlayerPos.row + 2, 0, sizeOfMaze);
+        currentPlayerPos.column = Math.Clamp(currentPlayerPos.column + 1, 0, sizeOfMaze);
         
-        nextMaelstromLocation.y = Math.Clamp(nextMaelstromLocation.y - 2, 0, sizeOfMaze);
-        nextMaelstromLocation.x = Math.Clamp(nextMaelstromLocation.x - 1, 0, sizeOfMaze);
+        nextMaelstromLocation.row = Math.Clamp(nextMaelstromLocation.row - 2, 0, sizeOfMaze);
+        nextMaelstromLocation.column = Math.Clamp(nextMaelstromLocation.column - 1, 0, sizeOfMaze);
 
         if (maelstroms[0] == previousMaelstromLocation)
             maelstroms[0] = nextMaelstromLocation;
         else
             maelstroms[1] = nextMaelstromLocation;
         
-        Console.WriteLine($"The player got sneezed by a maelstrom to location {playerPos}");
+        Console.WriteLine($"The player got sneezed by a maelstrom to location {currentPlayerPos}");
     }
 }
 
@@ -125,12 +128,12 @@ void DisplayCommandText()
     Console.WriteLine("=======");
 }
 
-void DisplayLocationMessage((int y, int x) playerLocation)
+void DisplayLocationMessage((int row, int column) playerLocation)
 {
-    Console.WriteLine($"You are in the room at (Row={playerLocation.y}, Column={playerLocation.x}).");
-    if (playerLocation is { y: 0, x: 0 })
+    Console.WriteLine($"You are in the room at (Row={playerLocation.row}, Column={playerLocation.column}).");
+    if (playerLocation == mazeEntrancePos)
         Console.WriteLine("You see light coming from the cavern entrance.");
-    else if (playerLocation is { y: 0, x: 2 })
+    else if (playerLocation == fountainLocation)
         Console.WriteLine(isFountainActivated
             ? "You hear the rushing waters from the Fountain of Objects. It has been reactivated!"
             : "You hear water dripping in this room. The Fountain of Objects is here!");
@@ -138,12 +141,12 @@ void DisplayLocationMessage((int y, int x) playerLocation)
 
 (int, int) CreateMaelstrom()
 {
-    (int y, int x) newMaelstrom = (0, 0);
+    (int row, int column) newMaelstrom = (0, 0);
     do
     {
-        newMaelstrom.y = random.Next(sizeOfMaze);
-        newMaelstrom.x = random.Next(sizeOfMaze);
-    } while (newMaelstrom is { x: 0, y: 0 } && newMaelstrom != fountainLocation);
+        newMaelstrom.row = random.Next(sizeOfMaze);
+        newMaelstrom.column = random.Next(sizeOfMaze);
+    } while (newMaelstrom == mazeEntrancePos || newMaelstrom == fountainLocation);
 
     return newMaelstrom;
 }
